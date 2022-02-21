6013cb4d47d91e5dee384f903bd523af0a9ddb25
Michael Fehrenbach <mfehrenbach@gmail.com>
2022-02-20 19:52:41 -0500

    Switch source code.

--- assets/demo.js
+++ assets/demo.js
@@ -2,12 +2,17 @@ const fileSelection = () => {
 	const visibleClass = 'visible'
 
 	const files = [...document.getElementById('files').children]
-	const source = document.getElementById('source')
+	const source = [...document.getElementById('source').children]
 
 	files.forEach((file) => {
 		(file).onclick = () => {
+			let index = files.indexOf(file)
+
 			files.forEach((element) => element.classList.remove(visibleClass))
+			source.forEach((element) => element.classList.remove(visibleClass))
+
 			file.classList.add(visibleClass)
+			source[index].classList.add(visibleClass)
 		}
 	})
 }
